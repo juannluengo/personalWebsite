5093e8da05582b7b8fb3220f9edb9e32b7d6aea4
juannluengo <juanluengogallego@gmail.com>
2025-03-23 17:41:25 +0100

    Fifth try

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',  // Enable static exports
-  distDir: 'out',
-  // Configure images for static export
   images: {
-    loader: 'custom',
-    loaderFile: './image-loader.js',
     unoptimized: true,
   },
-  // Add basePath for GitHub Pages (replace 'your-repo-name' with your actual repo name)
   basePath: process.env.NODE_ENV === 'production' ? '/personalWebsite' : '',
-  // Disable server-side features
-  trailingSlash: true,
   assetPrefix: process.env.NODE_ENV === 'production' ? '/personalWebsite' : '',
 };
 
